f6bdcfa971a0961a172f6f68ca2338811d96f0c5
acomarcho <acoxstpd@gmail.com>
2023-11-16 21:39:06 +0700

    Feat: Bootstrap create article page

---
+++ pages/article/create/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Divider, Stack, Title } from "@mantine/core";
+
+export default function CreateArticlePage() {
+  return (
+    <Container p={16}>
+      <Stack>
+        <Title order={1}>Buat artikel</Title>
+        <Divider />
+      </Stack>
+    </Container>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, Stack, Button } from "@mantine/core";
+import { Container, Title, Stack, Button, Divider } from "@mantine/core";
 import Router from "next/router";
 
 export default function IndexPage() {
@@ -6,6 +6,7 @@ export default function IndexPage() {
     <Container p={16}>
       <Stack align="flex-start">
         <Title order={1}>Dashboard Impresi</Title>
+        <Divider />
         <Title order={2}>Produk</Title>
         <Button
           onClick={() => {
@@ -13,6 +14,14 @@ export default function IndexPage() {
           }}
         >
           Buat produk
+        </Button>
+        <Title order={2}>Artikel</Title>
+        <Button
+          onClick={() => {
+            Router.push("/article/create");
+          }}
+        >
+          Buat artikel
         </Button>
       </Stack>
     </Container>
