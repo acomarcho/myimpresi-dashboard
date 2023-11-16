b697701daf8a2355c2d2eb2c5172a80946c1a63f
acomarcho <acoxstpd@gmail.com>
2023-11-16 21:43:41 +0700

    Feat: Prepare add article page

--- pages/article/create/index.tsx
+++ pages/article/create/index.tsx
@@ -1,4 +1,13 @@
-import { Container, Divider, Stack, Title } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Divider,
+  FileInput,
+  Stack,
+  TextInput,
+  Textarea,
+  Title,
+} from "@mantine/core";
 
 export default function CreateArticlePage() {
   return (
@@ -6,6 +15,16 @@ export default function CreateArticlePage() {
       <Stack>
         <Title order={1}>Buat artikel</Title>
         <Divider />
+        <TextInput label="Judul artikel" required />
+        <Textarea label="Konten artikel" required autosize minRows={2} />
+        <FileInput
+          label="Gambar artikel"
+          clearable
+          placeholder="Pilih gambar artikel"
+          required
+        />
+        <Divider />
+        <Button>Buat artikel</Button>
       </Stack>
     </Container>
   );
